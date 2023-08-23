64edbc2b754e95ff9cca625481fb5fb448442905
chaeyeonss <rccodus@gmail.com>
2023-08-24 05:13:20 +0900

    2023-08-24 프론트 동작 흐름 확인용 코드 작성 (인트로 로고 변경)

--- app/src/main/java/com/example/apptest/ReportedActivity.java
+++ app/src/main/java/com/example/apptest/ReportedActivity.java
@@ -3,11 +3,20 @@ package com.example.apptest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.ArrayList;
+
 public class ReportedActivity extends AppCompatActivity {
+
+    ArrayList<ReportedItem> reportedItemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,25 @@ public class ReportedActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("내 차 신고된 내역");
+
+        this.initializeList();
+
+        TextView date = (TextView) findViewById(R.id.reported_detail_date);
+        TextView address = (TextView) findViewById(R.id.reported_detail_address);
+        TextView status = (TextView) findViewById(R.id.reported_detail_car);
+
+
+        ListView listView = (ListView) findViewById(R.id.reported_table);
+        final ReportedListAdapter adapter = new ReportedListAdapter(this, reportedItemList);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                date.setText(adapter.getItem(position).getDate());
+                address.setText(adapter.getItem(position).getLongitube());
+                status.setText(adapter.getItem(position).getId());
+            }
+        });
 
 
         //Intent intent = getIntent();
@@ -66,6 +94,14 @@ public class ReportedActivity extends AppCompatActivity {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void initializeList() {
+        reportedItemList = new ArrayList<ReportedItem>();
+
+        reportedItemList.add(new ReportedItem("2023.03.01. 17:32:33","17:38:20","충청남도 아산시 중앙로 17","12모3456","1","고지서 발송 D+12","승인"));
+        reportedItemList.add(new ReportedItem("2023.06.06. 13:02:58","13:10:03","충청남도 아산시 순천향로","34가5421","2","신고 반려됨","반려"));
+        reportedItemList.add(new ReportedItem("2023.08.19. 19:48:03","20:01:16","인천광역시 연수구 선학로 65","82서8275","3","접수 중","접수 중"));
     }
 
 }
